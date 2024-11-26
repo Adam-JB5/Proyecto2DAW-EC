fa730056adb8efb035dff0601d003454d0666e5f
DAW204 <ajanahb02@educantabria.es>
2024-11-26 22:44:40 +0100

    Funcion para pintar las casillas disponibles para el movimiento del personaje creada. Pagina responsive. Funciones simplificadas. Algunos comentarios modificados

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-//Recoleccion de elementos HTML
+"use strict";
+
+document.addEventListener("DOMContentLoaded", inicio);
+
+function inicio() {
+
+
+
+/*Recoleccion de elementos del DOM*/
 
 //Div titulo
 let titulo = document.getElementById("titulo");
@@ -14,6 +22,14 @@ let dadoButton = document.getElementById("dadoButton");
 let dadoImg = document.getElementById("dadoImg");
 let jugarButton = document.getElementById("jugarButton");
 
+//Casilla del heroe
+let casillaHeroe;
+
+//Tablero
+let tablero = document.getElementById("tablero");
+
+
+
 //Variable de control usada en las funciones agitarDado() y tirarDado()
 let dadoAgitado = false;
 
@@ -21,10 +37,8 @@ let dadoAgitado = false;
 let rutaSprite = "./img/Character2Alt1.gif";
 
 
-//Se ha pulsado el boton del nombre
-nombreButton.addEventListener("click", validarNombre);
-
 //Esta funcion valida si el nombre tiene el numero correcto de caracteres
+nombreButton.addEventListener("click", validarNombre);
 function validarNombre() {
     //Recogo el valor del input del nombre y creo una expresion regular que evita numeros y permite letras con tilde y espacios
     let nombreUsuario = document.getElementById("nombreUsuario").value;
@@ -55,24 +69,24 @@ function validarNombre() {
     
 }
 
+
 //Cambio de color las interfaces de juego y pongo un fondo al tablero con una imagen
 function cambiarColor() {
     document.getElementById("interfazNombre").style.backgroundColor = "#6C697C";
     document.getElementById("interfazJuego").style.backgroundColor = "#9082D1";
-    document.getElementById("tablero").style.backgroundImage = "url(./img/Desert1.png)";
+    tablero.style.backgroundImage = "url(./img/Desert1.png)";
 }
 
 
 
 
-//Se ha pulsado el boton "Jugar"
-jugarButton.addEventListener("click", empezarJuego);
-
 //Esta funcion ejecuta varias funciones que dan lugar al comienzo del juego
+jugarButton.addEventListener("click", empezarJuego);
 function empezarJuego() {
     generarTablero();
     dibujarMenus();
 }
+
 
 //Se revela el dado y su boton y se oculta el boton "Jugar". Si el display de un elemento es "none" no se muestra, si es "block" se muestra de forma predeterminada
 function dibujarMenus() {
@@ -87,9 +101,48 @@ function dibujarMenus() {
 }
 
 
-//Se ha cambiado de opcion en la lista
-personalizarLista.addEventListener("change", cambiarPersonaje);
+//Esta funcion genera un tablero de 10x10 de divs
+function generarTablero() {
+    
 
+    //Con 2 ciclos for anidados genero 10 filas y 10 columnas de divs
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            
+            //Creo un elemento div y le añado una clase y un id con el siguiente formato: Nºfila,Nºcolumna
+            const celda = document.createElement("div");
+            celda.classList.add("celdaDiv");
+            celda.setAttribute("id", `${i},${j}`);
+
+            //Genero un numero aleatorio que corresponderá a una imagen distinta
+            let random = Math.floor(Math.random() * 6 + 1);
+            celda.style.backgroundImage = `url(./img/Desert${random}.png)`;
+
+            //Annado una celda, se annadira una por cada ciclo por lo que se acabaran dibujando un total de 100 divs (casillas)
+            tablero.appendChild(celda);
+        }
+        
+    }
+
+    //Ejecuto la funcion dibujarElementos que dibuja al heroe y el cofre en sus casillas correspondientes
+    dibujarElementos();
+}
+
+
+//Esta funcion dibuja al heroe y el cofre en sus casillas correspondientes
+function dibujarElementos() {
+    //Recogo las casillas correspondientes a donde apareceran el heroe y el cofre
+    casillaHeroe = document.getElementById("0,0");
+    let casillaCofre = document.getElementById("9,9");
+
+    //Uso innerHTML para insertar una etiqueta img que contendra la imagen respectiva del personaje y del cofre
+    casillaHeroe.innerHTML = `<img id="spritePersonaje" src='${rutaSprite}' alt=''>`;
+    casillaCofre.innerHTML = `<img id="spriteCofre" src='./img/Chest1.png' alt=''>`;
+}
+
+
+//Esta funcion cambia el sprite del personaje segun la eleccion del usuario, usando el evento "change"
+personalizarLista.addEventListener("change", cambiarPersonaje);
 function cambiarPersonaje() {
     let sprite = document.getElementById("spritePersonaje");
     let color = personalizarLista.value;
@@ -125,53 +178,7 @@ function cambiarPersonaje() {
     }
 }
 
-//Esta funcion genera un tablero de 10x10 de divs
-function generarTablero() {
 
-    //Recogo la seccion en la que se dibujaran los divs
-    let tablero = document.getElementById("tablero");
-    
-
-    //Con 2 ciclos for anidados genero 10 filas y 10 columnas de divs
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            
-            //Creo un elemento div y le añado una clase y un id con el siguiente formato: Nºfila,Nºcol
-            const celda = document.createElement("div");
-            celda.classList.add("celdaDiv");
-            celda.setAttribute("id", `${i},${j}`);
-
-            //Genero un numero aleatorio que corresponderá a una imagen distinta
-            let random = Math.floor(Math.random() * 6 + 1);
-            celda.style.backgroundImage = `url(./img/Desert${random}.png)`;
-
-            //Annado una celda, se annadira una por cada ciclo por lo que se acabaran dibujando un total de 100 divs (casillas)
-            tablero.appendChild(celda);
-        }
-        
-    }
-
-    //Ejecuto la funcion dibujarElementos que dibuja al heroe y el cofre en sus casillas correspondientes
-    dibujarElementos();
-}
-
-
-//Cada vez que se modifique el tamaño de la pantalla se vuelven a dibujar los elementos para adaptar el tamaño de las imagenes
-window.addEventListener("resize", dibujarElementos);
-
-//Esta funcion dibuja al heroe y el cofre en sus casillas correspondientes
-function dibujarElementos() {
-    //Recogo las casillas correspondientes a donde apareceran el heroe y el cofre
-    let casillaHeroe = document.getElementById("0,0");
-    let casillaCofre = document.getElementById("9,9");
-
-    //Recogo la altura de cada casilla con getComputedStyle, que devuelve un string con este formato: 55px. Ya que devuelve un string uso split para recoger solamente el numero
-    let alturaCasilla = window.getComputedStyle(casillaCofre).height.split("p")[0];
-
-    //Les meto a las casillas una etiqueta img con los atributos src y style, en la que les pongo que la altura maxima sera el 80% de la altura de las casillas y el minimo el 50%
-    casillaHeroe.innerHTML = `<img id="spritePersonaje" src='${rutaSprite}' alt='' style='max-height: ${alturaCasilla * 0.8}px; min-height: ${alturaCasilla * 0.5}px;'>`;
-    casillaCofre.innerHTML = `<img id="spriteCofre" src='./img/Chest1.png' alt='' style='max-height: ${alturaCasilla * 0.8}px; min-height: ${alturaCasilla * 0.5}px;'>`;
-}
 
 
 
@@ -195,8 +202,42 @@ function tirarDado() {
         console.log(random);
         dadoImg.setAttribute("src", `./img/DadoP${random}.png`);
         dadoAgitado = false;
+
+        pintarCasillas(random);
     }
     
     
 }
 
+
+function pintarCasillas(num) {
+    casillaHeroe.style.backgroundColor = "grey";
+
+    let opcion1 = document.getElementById(`${parseInt(casillaHeroe.id.split(",")[0]) + num},${casillaHeroe.id.split(",")[1]}`);
+    let opcion2 = document.getElementById(`${casillaHeroe.id.split(",")[0]},${parseInt(casillaHeroe.id.split(",")[1]) + num}`);
+
+    console.log(opcion1);
+    console.log(opcion2);
+
+    opcion1.style.backgroundColor = "red";
+    opcion2.style.backgroundColor = "red";
+
+    opcion1.style.backgroundImage = "unset";
+    opcion2.style.backgroundImage = "unset";
+}
+
+
+tablero.addEventListener("click", seleccionarCasilla)
+
+function seleccionarCasilla(ev) {
+    let casillaSeleccionada = ev.target;
+
+
+}
+
+
+
+
+
+
+}
